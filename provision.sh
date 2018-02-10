b15896fab58842794f1256626b9be8a4a6c447a0
hemanth22 <hemanth22hemu@gmail.com>
2018-02-10 13:42:00 +0530

    Correcting provision and travis ci yml file

--- provision.sh
+++ provision.sh
@@ -1,5 +1,7 @@
 usermod -aG wheel vagrant
 
+#yum makecache fast
+
 yum -y install epel-release
 
 yum -y install vim sl nginx
@@ -7,5 +9,13 @@ yum -y install vim sl nginx
 systemctl enable nginx
 
 systemctl start nginx
+
+systemctl start firewalld
+
+systemctl status -l firewalld
+
+firewall-cmd --add-port=8080/tcp --permanent
+
+firewall-cmd --reload
 
 cp -r /vagrant/www/index.html /usr/share/nginx/html/index.html
